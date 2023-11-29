5931b0c30b6189827ad845380ef525a328548424
Agustin Peres <agu.peres3@gmail.com>
2023-11-29 17:49:44 -0300

    changed the example URL on astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import solid from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import solid from "@astrojs/solid-js";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.example.com',
-  output: 'hybrid',
-  integrations: [react({
-    include: ['**/react/*']
-  }), solid({
-    include: ['**/solid/*']
-  })],
-  adapter: netlify()
+  site: "https://main--gregarious-daifuku-be8bf4.netlify.app/",
+  output: "hybrid",
+  integrations: [
+    react({
+      include: ["**/react/*"],
+    }),
+    solid({
+      include: ["**/solid/*"],
+    }),
+  ],
+  adapter: netlify(),
 });
